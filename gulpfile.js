a7da2536e1c95d4e139790ad3c283ba91a3b3870
Дмитрий Халин <dimaskhln@gmail.com>
2021-02-05 19:32:42 +0700

    Add files

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+var pug = require('gulp-pug');
+
+gulp.task('views', function buildHTML() {
+  return gulp.src('src/pages/**/*.pug').pipe(pug());
+});
+
+gulp.task('sasssass', function () {
+  return gulp
+    .src('/src/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
