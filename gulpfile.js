028d696ccaccc76653c62a6ab13e1fa44cd54e68
Дмитрий Халин <dimaskhln@gmail.com>
2021-02-09 22:42:06 +0700

    Watch and live reload! Also icon attribution added

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,35 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+const { series } = require('gulp');
+
+const { watch } = require('gulp');
 
 sass.compiler = require('node-sass');
 
 var pug = require('gulp-pug');
 
 gulp.task('views', function buildHTML() {
-  return gulp.src('src/pages/*.pug').pipe(pug()).pipe(gulp.dest('build'));
+  return gulp
+    .src('src/pages/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('build'))
+    .pipe(livereload());
 });
 
 gulp.task('sass', function () {
-  return gulp.src('src/styles/*.scss').pipe(sass()).pipe(gulp.dest('build'));
+  return gulp
+    .src('src/styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build'))
+    .pipe(livereload());
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch('src/pages/**/*.scss', gulp.series('sass'));
+  gulp.watch('src/pages/**/*.pug', gulp.series('views'));
 });
+
+exports.start = series('watch');
